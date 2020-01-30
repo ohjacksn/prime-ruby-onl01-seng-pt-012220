9eb92744909da3cc081f6ceb0454cdfb17875a3e
ohjacksn <jacksonmo@me.com>
2020-01-30 15:05:53 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+=begin
+def prime?(integer)
+    i = 2
+    while i < integer
+        false if integer % i == 0
+        i += 1
+    end
+    true
+end
+=end
+def prime?(integer)
+    i = 2
+    return false if integer <= 1
+    while i < integer
+        return false if integer % i == 0
+        i += 1
+    end
+    true
+end
